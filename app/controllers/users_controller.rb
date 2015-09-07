ffb44f736b81234f19f6c096984212113752d896
SarahNox <apnoeist@googlemail.com>
2015-09-07 15:25:16 +0200

    railstutorial chapter 7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])    # Not the final implementation!
+    @user = User.new(params[:user]) 
     if @user.save
-      # Handle a successful save.
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
